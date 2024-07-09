741a1bcfd11307512be8f12386281f52ffbd10c0
bbear2 <bbear_mail@163.com>
2024-07-09 23:08:15 +0800

    optimize the implementation of Log_Printf
    Signed-off-by: bbear2 <bbear_mail@163.com>

--- source_code/Core/Src/log.c
+++ source_code/Core/Src/log.c
@@ -1,5 +1,6 @@
 #include "log.h"
 #include <stdarg.h>
+#include <string.h>
 
 void Log_Init(void)
 {
@@ -8,25 +9,73 @@ void Log_Init(void)
 #endif
 }
 
-int8_t Log_Printf(char *fmt, ...)
+void itoa(int value, char *str, int base) {
+    char *ptr = str, *ptr1 = str, tmp_char;
+    int tmp_value;
+    if (base < 2 || base > 36) {
+        *str = '\0';
+        return;
+    }
+    do {
+        tmp_value = value;
+        value /= base;
+        *ptr++ = "0123456789abcdefghijklmnopqrstuvwxyz"[tmp_value - value * base];
+    } while (value);
+
+    if (tmp_value < 0 && base == 10) *ptr++ = '-';
+    *ptr-- = '\0';
+
+    while (ptr1 < ptr) {
+        tmp_char = *ptr;
+        *ptr-- = *ptr1;
+        *ptr1++ = tmp_char;
+    }
+}
+
+int8_t Log_Printf(char *format, ...)
 {
 #if DEBUG_LOG
     char log_buf[MAX_LOG_BUF_LEN] = { 0 };
-    uint32_t i = 0;
-    uint32_t length = 0;
-    uint8_t time_cnt = 0;
+    char *buf_ptr = log_buf;
+    uint16_t time_cnt = 0;
 
-    va_list arg_ptr;
-    va_start(arg_ptr, fmt);
-    length = vsnprintf(log_buf, MAX_LOG_BUF_LEN, fmt, arg_ptr);
-    if(length > sizeof(log_buf) - 1) {
-        length = sizeof(log_buf) - 1;
-        log_buf[length] = '\0';
+    va_list args;
+    va_start(args, format);
+
+    while (*format) {
+        if (*format == '%') {
+            format++;
+            if (*format == 'd') {
+                int value = va_arg(args, int);
+                char temp[16];
+                itoa(value, temp, 10); 
+                strcpy(buf_ptr, temp);
+                buf_ptr += strlen(temp);
+            } else if (*format == 'x') {
+                int value = va_arg(args, int);
+                char temp[16];
+                itoa(value, temp, 16); 
+                strcpy(buf_ptr, temp);
+                buf_ptr += strlen(temp);
+            } else if (*format == 's') {
+                char *str = va_arg(args, char*);
+                strcpy(buf_ptr, str);
+                buf_ptr += strlen(str);
+            } else {
+                *buf_ptr++ = '%';
+                *buf_ptr++ = *format;
+            }
+        } else {
+            *buf_ptr++ = *format;
+        }
+        format++;
     }
-    va_end(arg_ptr);
+    *buf_ptr = '\0';
 
-    while(i < length) {
-        LL_USART_TransmitData8(USART2, log_buf[i++]);
+    va_end(args);
+
+    for (char *p = log_buf; *p; p++) {
+        LL_USART_TransmitData8(USART2, *p);
         while(LL_USART_IsActiveFlag_TC(USART2) == 0){
             LL_mDelay(1);
             if(time_cnt++ > 1000)
@@ -36,3 +85,4 @@ int8_t Log_Printf(char *fmt, ...)
 #endif
     return 0;
 }
+
